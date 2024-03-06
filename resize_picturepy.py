e70e8f5bb843316e9278d23a2af785ad7f0f2788
suzu6331 <128467874+suzu6331@users.noreply.github.com>
2024-03-06 18:09:43 +0900

    Add files via upload

--- resize_picturepy.py
+++ resize_picturepy.py
@@ -18,3 +18,23 @@ def form():
         </html>
     '''
 
+
+def resize_picture():
+    image_file = request.files['image']
+    target_size_mb = float(request.form['target_size'])
+    target_size = target_size_mb * 1024 * 1024  #バイトに変換
+
+    img = Image.open(image_file)
+    buffer = io.BytesIO()
+    
+    # 画質を下げながら調整
+    for quality in range(100,0,-5):
+        buffer.seek(0)
+        img.save(buffer,'JPEG',quality=quality)
+        size = buffer.tell()
+        if size <= target_size : break
+    buffer.seek(0)
+    return send_file(buffer, as_attachment=True, download_name='resized_image.jpg', mimetype='image/jpeg')
+
+if __name__ == '__main__':
+    app.run(debug=True)
